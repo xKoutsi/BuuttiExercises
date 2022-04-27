216b452c3b1c075a46ccdd038fa7a480266f0b18
xKoutsi <90937165+xKoutsi@users.noreply.github.com>
2022-04-27 09:32:22 +0300

    filenames changed
    Changed filenames to find right task faster.

---
+++ arrays.js
@@ -0,0 +1,19 @@
+let arr = ["banaani", "omena", "mandariini", "appelsiini", "kurkku", "tomaatti", "peruna"];
+
+console.log(arr[2]);                // Print 3rd and 5th items on array
+console.log(arr[4]);
+console.log(arr.length);            // Print array length
+arr.push("sipuli");                 // Add sipuli to array
+
+arr.shift();                        // remove the first item on array
+console.log(arr);
+
+arr.forEach(element => {
+    console.log(element);
+});
+
+arr.forEach(element => {
+    if(element.includes("r")){
+        console.log(element);
+    }
+});
